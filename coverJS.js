7db6f02837a837e786ee5c3ec3a15505876719e5
Abhimanyu Dayal <abhimanyudayal1@gmail.com>
2022-05-19 11:00:47 +0530

    new plan, new exec

--- coverJS.js
+++ coverJS.js
@@ -1,16 +1,55 @@
-var coverPage = document.getElementById("bg");
+// FADE SCREEN 
 
-coverPage.addEventListener("scroll", scroll_funct);
+var bleh = document.getElementById("front-screen-stuff");
+var scrollButton = document.getElementById("scroll-object");
+let a; 
 
-function scroll_funct(){
-    
-    var opa = Number(window.getComputedStyle(coverPage).getPropertyValue("opacity"));
-    console.log("nerf herder");
-    
-    if(opa != 0){
-        opa -= 0.0315;
-        coverPage.style.opacity = opa;
-    } else{
-        coverPage.removeEventListener("scroll", scroll_funct);
+scrollButton.addEventListener("click", temp);
+
+function temp(){
+    if(!a){
+        a = setInterval(opac, 10);
     }
+}
+
+function opac(){
+    
+    var opa = Number(window.getComputedStyle(bleh).getPropertyValue("opacity"));
+    if(opa != 0){
+        opa -= 0.01;
+        bleh.style.opacity = opa; 
+    } else{
+        clearInterval(a);
+        a = null;
+        document.getElementById("front-screen-stuff").remove();
+    }
+}
+
+// SLIDESHOW
+
+let slideIndex = 1;
+showSlides(slideIndex);
+
+function plusSlides(n) {
+    showSlides(slideIndex += n);
+}
+
+function currentSlide(n) {
+    showSlides(slideIndex = n);
+}
+
+function showSlides(n) {
+    let i;
+    let slides = document.getElementsByClassName("mySlides");
+    let dots = document.getElementsByClassName("dot");
+    if (n > slides.length) {slideIndex = 1}
+    if (n < 1) {slideIndex = slides.length}
+    for (i = 0; i < slides.length; i++) {
+    slides[i].style.display = "none";
+    }
+    for (i = 0; i < dots.length; i++) {
+    dots[i].className = dots[i].className.replace(" active", "");
+    }
+    slides[slideIndex-1].style.display = "block";
+    dots[slideIndex-1].className += " active";
 }
